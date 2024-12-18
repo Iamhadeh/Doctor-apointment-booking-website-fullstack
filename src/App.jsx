e3e6e5bc24981062c99501cb39cbb7f5e477be7e
ADEKUSIBE OGUNMOKUN <cusibeade@gmail.com>
2024-12-18 15:06:08 +0100

    starting

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+
+
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import Doctors from './pages/Doctors'
+import Login from './pages/Login';
+import MyProfile from './pages/MyProfile';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import MyAppointment from './pages/MyAppointment';
+import Appointment from './pages/Appointment';
+import Navbar from './components/Navbar';
+
+ const App = () => {
+  return (
+    <div className='mx-4 sm:{10%}' >
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/doctors' element={<Doctors />}/>
+        <Route path='/doctors:speciality' element={<Doctors />}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/my-profile' element={<MyProfile/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/my-appiontment' element={<MyAppointment/>}/>
+        <Route path='/appointment:doctorsID' element={<Appointment />}/>
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ src/pages/Doctors.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+const Doctors = () => {
+  return (
+    <div>
+      <h1>Welcome to doctor's page</h1>
+        
+    </div>
+  )
+}
+
+export default Doctors;

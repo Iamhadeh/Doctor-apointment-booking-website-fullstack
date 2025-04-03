48ecb0d140e92e840103ea05b54c7f719651f845
ADEKUSIBE OGUNMOKUN <cusibeade@gmail.com>
2025-04-03 23:51:35 +0100

    book appointment section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Speciality from '../components/Speciality'
 import TopDoctors from '../components/TopDoctors'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Header/>
       <Speciality/>
       <TopDoctors/>
+      <Banner/>
     </div>
   )
 }
